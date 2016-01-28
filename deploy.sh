21c7c0f99389062be4c7ec2a0ce7b66f5c583c78
Scott Hardy <scott.the.hardy@gmail.com>
2016-01-27 20:41:49 -0800

    travis try 4

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ git config user.name "Travis CI"
 git config user.email "travis@travis-ci.org"
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
