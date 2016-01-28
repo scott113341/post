72d62c3eb5fac14861ea08b7b251fafea937eb6d
Scott Hardy <scott.the.hardy@gmail.com>
2016-01-28 10:26:54 -0800

    removed github token from output and regenerated token

--- deploy.sh
+++ deploy.sh
@@ -13,4 +13,4 @@ git config user.email "travis@travis-ci.org"
 git add .
 git add -f build/
 git commit -m "Deploy ${COMMIT} to GitHub Pages"
-git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
