2aeea4569d000023a15cfca9e1eaeb110b200d10
Scott Hardy <scott.the.hardy@gmail.com>
2016-01-28 10:22:28 -0800

    added master commit hash to deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,12 @@ mkdir build
 
 npm run build
 
+COMMIT=$(git log -1 | cat | head -n 1)
 rm -rf .git
 git init
 git config user.name "Travis CI"
 git config user.email "travis@travis-ci.org"
 git add .
 git add -f build/
-git commit -m "Deploy to GitHub Pages"
+git commit -m "Deploy ${COMMIT} to GitHub Pages"
 git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
